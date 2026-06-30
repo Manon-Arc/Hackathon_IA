07c8f64b92e677d8941046b8ad7d2cd64ad3fc77
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2026-06-30 16:20:39 +0200

    [FIX] port

--- rendu/devweb/backend/run.py
+++ rendu/devweb/backend/run.py
@@ -9,5 +9,5 @@ from app import create_app
 app = create_app(os.getenv('FLASK_ENV', 'development'))
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 5005))
     app.run(host='0.0.0.0', port=port, debug=app.debug)
